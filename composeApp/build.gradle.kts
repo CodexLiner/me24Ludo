325957a78675ccaf1839a4b2e2eba359144c50fe
Gopal Meena <meenagopal24@gmail.com>
2025-03-17 12:29:26 +0530

    Refactor: Update `lifecycle-viewmodel-compose` and `compottie` dependencies
    - Update `lifecycle-viewmodel-compose` and `compottie` dependencies to use the libraries block.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -28,8 +28,8 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            implementation("org.jetbrains.androidx.lifecycle:lifecycle-viewmodel-compose:2.8.2")
-            implementation("io.github.alexzhirkevich:compottie:2.0.0-rc04")
+            implementation(libs.lifecycle.viewmodel.compose)
+            implementation(libs.compottie)
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
